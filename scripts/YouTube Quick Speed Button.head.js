ed4473a7dd776b29cce970a9b2e11e6fc432f37b
OddMorning <OddMorning@ya.ru>
2020-04-01 14:08:43 +0300

    Update YouTube Quick Speed Button.head.js

--- scripts/YouTube Quick Speed Button.head.js
+++ scripts/YouTube Quick Speed Button.head.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name         YouTube Quick Speed Button
-// @version      1.6.1
-// @date         2020-03-31
+// @version      1.6.2
+// @date         2020-04-01
 // @author       OddMorning
 // @description  Script adds a custom button for setting playback speed
 // @match        https://www.youtube.com/*
 // @icon         https://s.ytimg.com/yts/img/favicon_144-vfliLAfaB.png
-// @updateURL    https://github.com/OddMorning/TamperMonkey-Scripts/raw/master/scripts/YouTube%20Quick%20Speed%20Button.head.js
-// @downloadURL  https://github.com/OddMorning/TamperMonkey-Scripts/raw/master/scripts/YouTube%20Quick%20Speed%20Button.user.js
+// @updateURL    https://pastebin.com/raw/rTNnGYt1
+// @downloadURL  https://pastebin.com/raw/2hBRUjzE
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_addStyle
 // @grant        GM_registerMenuCommand
-// @require      https://raw.github.com/odyniec/MonkeyConfig/master/monkeyconfig.js
+// @require      https://raw.githubusercontent.com/OddMorning/TamperMonkey-Scripts/master/lib/monkeyconfig-lite.js
 // ==/UserScript==
