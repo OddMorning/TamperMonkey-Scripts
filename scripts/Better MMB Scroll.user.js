e6eb04235fa06e13bc1e9b2c3eb7e8d3633aff3c
OddMorning <OddMorning@ya.ru>
2020-04-01 14:11:51 +0300

    Update Better MMB Scroll.user.js

--- scripts/Better MMB Scroll.user.js
+++ scripts/Better MMB Scroll.user.js
@@ -1,31 +1,30 @@
 // ==UserScript==
 // @name         Better MMB Scroll
-// @version      1.1
-// @date         2020-03-31
+// @version      1.1.2
+// @date         2020-04-01
 // @author       OddMorning
 // @description  Hold MMB and move mouse to scroll up and down. Inspired by Everything's scrolling
 // @match        http*://*/*
 // @icon         https://cdn3.iconfinder.com/data/icons/iconic-1/32/move_alt1-512.png
-// @updateURL    https://github.com/OddMorning/TamperMonkey-Scripts/raw/master/scripts/Better%20MMB%20Scroll.head.js
-// @downloadURL  https://github.com/OddMorning/TamperMonkey-Scripts/raw/master/scripts/Better%20MMB%20Scroll.user.js
+// @updateURL    https://github.com/OddMorning/TamperMonkey-Scripts/raw/master/scripts/YouTube%20Quick%20Speed%20Button.head.js
+// @downloadURL  https://github.com/OddMorning/TamperMonkey-Scripts/raw/master/scripts/YouTube%20Quick%20Speed%20Button.user.js
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_addStyle
 // @grant        GM_registerMenuCommand
-// @require      https://raw.github.com/odyniec/MonkeyConfig/master/monkeyconfig.js
+// @require      https://raw.githubusercontent.com/OddMorning/TamperMonkey-Scripts/master/lib/monkeyconfig-lite.js
 // ==/UserScript==
 'use strict';
 
 const Config = new MonkeyConfig({
-    title: 'Configure',
-    menuCommand: true,
-    params: {
-        scroll_speed: {
-            label: 'Scroll Speed (Default: 10)',
-            type: 'number',
-            default: 10,
-        }
+  title: 'Configure',
+  params: {
+    scroll_speed: {
+      label: 'Scroll Speed',
+      type: 'number',
+      default: 10,
     }
+  }
 })
 
 const styleStr = `cursor: ns-resize !important`
@@ -72,3 +71,4 @@ document.addEventListener('mouseup', (ev) => {
   document.removeEventListener('mousemove', onMouseMove)
   onLeaveMode(ev)
 })
+
