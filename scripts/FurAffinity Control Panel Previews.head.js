dcbab4372e2ffe28c86aac7b7c6f9385686f3f9a
OddMorning <OddMorning@ya.ru>
2020-04-01 13:52:20 +0300

    Create FurAffinity Control Panel Previews.head.js

---
+++ scripts/FurAffinity Control Panel Previews.head.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         FurAffinity CP Previews
+// @version      1.1
+// @date         2019-09-31
+// @description  Adds thumbnails for favs and readable comments in the user control panes
+// @author       OddMorning
+// @icon         http://www.furaffinity.net/themes/beta/img/favicon.ico
+// @match        http*://www.furaffinity.net/*
+// @grant        GM_addStyle
+// @grant        unsafeWindow
+// @updateURL    https://raw.githubusercontent.com/OddMorning/TamperMonkey-Scripts/master/scripts/FurAffinity%20Control%20Panel%20Previews.head.js
+// @downloadURL  https://raw.githubusercontent.com/OddMorning/TamperMonkey-Scripts/master/scripts/FurAffinity%20Control%20Panel%20Previews.user.js
+// @noframes
+// ==/UserScript==
