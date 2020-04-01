f0851f1751b3b745b949fa25542d63626292abed
OddMorning <OddMorning@ya.ru>
2020-04-01 13:38:27 +0300

    Update @updateURL and @downloadURL

--- scripts/Ava's Demon pages counter.user.js
+++ scripts/Ava's Demon pages counter.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
-// @name        Ava's Demon pages counter
-// @version     1.1.2
-// @date        2019-07-18
-// @author      OddMorning
-// @match       https://avasdemon.com/pages*
-// @match       https://www.avasdemon.com/pages*
-// @icon        https://avasdemon.com/favicon.ico
-// @grant       none
-// @run-at      document-start
-// @updateURL   ...
-// @downloadURL ...
+// @name         Ava's Demon pages counter
+// @version      1.1.2
+// @date         2019-07-18
+// @author       OddMorning
+// @match        https://avasdemon.com/pages*
+// @match        https://www.avasdemon.com/pages*
+// @icon         https://avasdemon.com/favicon.ico
+// @grant        none
+// @run-at       document-start
+// @updateURL    https://github.com/OddMorning/TamperMonkey-Scripts/raw/master/scripts/Ava's%20Demon%20pages%20counter.head.js
+// @downloadURL  https://github.com/OddMorning/TamperMonkey-Scripts/raw/master/scripts/Ava's%20Demon%20pages%20counter.user.js
 // ==/UserScript==
 'use strict';
 
