fd717ab04c26e017b24766607bce57ca97e0de70
OddMorning <OddMorning@ya.ru>
2020-04-01 13:59:56 +0300

    Update @updateURL and @downloadURL

--- scripts/Better MMB Scroll.user.js
+++ scripts/Better MMB Scroll.user.js
@@ -6,8 +6,8 @@
 // @description  Hold MMB and move mouse to scroll up and down. Inspired by Everything's scrolling
 // @match        http*://*/*
 // @icon         https://cdn3.iconfinder.com/data/icons/iconic-1/32/move_alt1-512.png
-// @updateURL    ...
-// @downloadURL  ...
+// @updateURL    https://github.com/OddMorning/TamperMonkey-Scripts/raw/master/scripts/Better%20MMB%20Scroll.head.js
+// @downloadURL  https://github.com/OddMorning/TamperMonkey-Scripts/raw/master/scripts/Better%20MMB%20Scroll.user.js
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_addStyle
