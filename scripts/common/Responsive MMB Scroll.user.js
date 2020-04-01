b36eb14cc7dd22dbf82d7705ef41312cbad60eee
OddMorning <OddMorning@ya.ru>
2020-04-01 16:38:08 +0300

    Rename script

--- scripts/common/Responsive MMB Scroll.user.js
+++ scripts/common/Responsive MMB Scroll.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name         Better MMB Scroll
+// @name         Responsive MMB Scroll
 // @version      1.1.2
 // @date         2020-04-01
 // @author       OddMorning
 // @description  Hold MMB and move mouse to scroll up and down. Inspired by Everything's scrolling
 // @match        http*://*/*
 // @icon         https://cdn3.iconfinder.com/data/icons/iconic-1/32/move_alt1-512.png
-// @updateURL    https://github.com/OddMorning/TamperMonkey-Scripts/raw/master/scripts/common/Better%20MMB%20Scroll.head.js
-// @downloadURL  https://github.com/OddMorning/TamperMonkey-Scripts/raw/master/scripts/common/Better%20MMB%20Scroll.user.js
+// @updateURL    https://github.com/OddMorning/TamperMonkey-Scripts/raw/master/scripts/common/Responsive%20MMB%20Scroll.head.js
+// @downloadURL  https://github.com/OddMorning/TamperMonkey-Scripts/raw/master/scripts/common/Responsive%20MMB%20Scroll.user.js
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_addStyle
