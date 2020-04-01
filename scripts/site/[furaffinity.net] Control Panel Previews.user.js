d5e71ccb70b8833cfdd13e63569279cda548d9f9
OddMorning <OddMorning@ya.ru>
2020-04-01 14:33:41 +0300

    Rename file

--- scripts/site/[furaffinity.net] Control Panel Previews.user.js
+++ scripts/site/[furaffinity.net] Control Panel Previews.user.js
@@ -1,17 +1,19 @@
 // ==UserScript==
-// @name         FurAffinity CP Previews
+// @name         [furaffinity.net] Control Panel Previews
 // @version      1.1
 // @date         2019-09-31
 // @description  Adds thumbnails for favs and readable comments in the user control panes
 // @author       OddMorning
+// @homepage     https://github.com/OddMorning/TamperMonkey-Scripts
 // @icon         http://www.furaffinity.net/themes/beta/img/favicon.ico
 // @match        http*://www.furaffinity.net/*
 // @grant        GM_addStyle
 // @grant        unsafeWindow
-// @updateURL    https://raw.githubusercontent.com/OddMorning/TamperMonkey-Scripts/master/scripts/site/FurAffinity%20Control%20Panel%20Previews.head.js
-// @downloadURL  https://raw.githubusercontent.com/OddMorning/TamperMonkey-Scripts/master/scripts/site/FurAffinity%20Control%20Panel%20Previews.user.js
+// @updateURL    https://github.com/OddMorning/TamperMonkey-Scripts/raw/master/scripts/site/%5Bfuraffinity.net%5D%20Control%20Panel%20Previews.head.js
+// @downloadURL  https://github.com/OddMorning/TamperMonkey-Scripts/raw/master/scripts/site/%5Bfuraffinity.net%5D%20Control%20Panel%20Previews.user.js
 // @noframes
 // ==/UserScript==
+
 /* jshint -W097 */
 /* jshint esnext: true */
 'use strict';
