5293720740d825100f671f7cc20030f7fb2156bb
OddMorning <OddMorning@ya.ru>
2020-04-01 14:00:03 +0300

    Create Better MMB Scroll.head.js

---
+++ scripts/Better MMB Scroll.head.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Better MMB Scroll
+// @version      1.1
+// @date         2020-03-31
+// @author       OddMorning
+// @description  Hold MMB and move mouse to scroll up and down. Inspired by Everything's scrolling
+// @match        http*://*/*
+// @icon         https://cdn3.iconfinder.com/data/icons/iconic-1/32/move_alt1-512.png
+// @updateURL    https://github.com/OddMorning/TamperMonkey-Scripts/raw/master/scripts/Better%20MMB%20Scroll.head.js
+// @downloadURL  https://github.com/OddMorning/TamperMonkey-Scripts/raw/master/scripts/Better%20MMB%20Scroll.user.js
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_addStyle
+// @grant        GM_registerMenuCommand
+// @require      https://raw.github.com/odyniec/MonkeyConfig/master/monkeyconfig.js
+// ==/UserScript==
