9b44d808ff734262b45f930b232f71c9205d7eea
OddMorning <OddMorning@ya.ru>
2020-04-01 14:57:03 +0300

    Update [youtube.com] Quick Speed Button.head.js

--- scripts/site/[youtube.com] Quick Speed Button.head.js
+++ scripts/site/[youtube.com] Quick Speed Button.head.js
@@ -13,5 +13,5 @@
 // @grant        GM_setValue
 // @grant        GM_addStyle
 // @grant        GM_registerMenuCommand
-// @require      https://raw.githubusercontent.com/OddMorning/TamperMonkey-Scripts/master/lib/monkeyconfig-lite.js
+// @require      https://raw.githubusercontent.com/OddMorning/TamperMonkey-Scripts/master/scripts/lib/monkeyconfig-lite.js
 // ==/UserScript==
